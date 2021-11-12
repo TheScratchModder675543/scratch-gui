721a633485c7948aa97b286d08a7325781bc59fa
TheScratchModder675543 <89365702+TheScratchModder675543@users.noreply.github.com>
2021-11-12 18:30:50 +0430

    Made blocks
    The blocks

---
+++ Blocks.js
@@ -0,0 +1,3 @@
+'r', 'letters %n through %n of %s', 'substringy', '2', '5', 'hello world'
+//breakdown below:
+'r' = block type, 'letters %n through %n of %s' = block text, 'substringy' = block ID/opcode
